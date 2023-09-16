da502507209159546db8a70dd49edbcac04935cb
Cristian <crisdela2001@gmail.com>
2023-09-16 04:04:05 -0400

    changes

--- django_project/settings.py
+++ django_project/settings.py
@@ -156,8 +156,6 @@ EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 DEFAULT_FROM_EMAIL = "crisdela2001@gmail.com"
 EMAIL_HOST = "smtp.sendgrid.net"
 EMAIL_HOST_USER = "apikey"
-EMAIL_HOST_PASSWORD = (
-    "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"
-)
+EMAIL_HOST_PASSWORD = env.str("EMAIL_HOST_PASSWORD")
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
